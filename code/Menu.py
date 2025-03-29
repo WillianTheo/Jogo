ea695a57828eb506439d7c16cdbcf7b13b1e84c3
Willian Theodoro <williantheodoro@hotmail.com>
2025-03-29 17:48:25 -0300

    Changing color menu

--- code/Menu.py
+++ code/Menu.py
@@ -18,8 +18,8 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, 'JOJO', COLOR_WHITE, ((WIN_WIDTH/2), 70))
-            self.menu_text(50, 'DO ELLAN', COLOR_WHITE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, 'JOJO', COLOR_BLUE, ((WIN_WIDTH/2), 70))
+            self.menu_text(50, 'DO ELLAN', COLOR_BLUE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
