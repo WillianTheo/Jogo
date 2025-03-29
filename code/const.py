12bf6682910b744b1438b44d5f33edbdb4913134
Willian Theodoro <williantheodoro@hotmail.com>
2025-03-29 17:44:08 -0300

    Add soundtrack and a image to menu

---
+++ code/const.py
@@ -0,0 +1,15 @@
+#C
+COLOR_BLUE = (66, 133, 244)
+COLOR_WHITE = (255, 255, 255)
+
+#m
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+#W
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
